be7462c58e62d545c8791edcbad098174536bcfe
Cameron Nye <csnye86@gmail.com>
2018-08-08 16:31:35 -0600

    cleaned up nav html

--- src/App.js
+++ src/App.js
@@ -64,58 +64,39 @@ class App extends Component {
       return (
         <div className="App" >
           <div className="navMain">
-            <div className="leftnav">
-              <h3 onClick={() => this.scrollToHome1()}>csn</h3>
+            <h3 onClick={() => this.scrollToHome1()}>csn</h3>
+            <div className="hamMenu" onClick={() => this.toggleMenuFn()}>
+              <span className={this.state.toggleMenu ? "line line01" : "line line01closed"}/>
+              <span className={this.state.toggleMenu ? "line line02" : "line line03closed"}/>
+              <span className={this.state.toggleMenu ? "line line03" : "line line03closed"}/>
             </div>
-            <div className="rightnav">
-              <div className="hamMenu" onClick={() => this.toggleMenuFn()}>
-                <span
-                  className={
-                    this.state.toggleMenu ? "line line01" : "line line01closed"
-                  }
-                />
-                <span
-                  className={
-                    this.state.toggleMenu ? "line line02" : "line line03closed"
-                  }
-                />
-                <span
-                  className={
-                    this.state.toggleMenu ? "line line03" : "line line03closed"
-                  }
-                />
-              </div>
-              <div
-                className={!this.state.toggleMenu ? "dropdown" : "hideDropdown"}
-              >
-                <ul>
-                  <li onClick={() => this.scrollToHome()} className="li1">
-                    Home
-                  </li>
-                  <li
-                    onClick={() => this.scrollToTechnologies()} className="li2">
-                    Technologies
-                  </li>
-                  <li onClick={() => this.scrollToProjects()} className="li3">
-                    Projects
-                  </li>
-                  <li onClick={() => this.scrollToAbout()} className="li4">
-                    About Me
-                  </li>
-                  <li onClick={() => this.scrollToContact()} className='li5'>
-                    Contact
-                  </li>
-                  <div>
-                    <a href="https://github.com/csnye86">
-                      <img className="social" src={githublogo} alt="" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/cameronnye/">
-                      <img className="social" src={linkedinlogo} alt="" />
-                    </a>
-                  </div>
-                  <li className='mobileSite'>*Mobile site coming soon</li>
-                </ul>
-              </div>
+            <div className={!this.state.toggleMenu ? "dropdown" : "hideDropdown"}>
+              <ul>                  
+                <li onClick={() => this.scrollToHome()} className="li1">
+                  Home
+                </li>
+                <li onClick={() => this.scrollToTechnologies()} className="li2">
+                  Technologies
+                </li>
+                <li onClick={() => this.scrollToProjects()} className="li3">
+                  Projects
+                </li>
+                <li onClick={() => this.scrollToAbout()} className="li4">
+                  About Me
+                </li>
+                <li onClick={() => this.scrollToContact()} className='li5'>
+                  Contact
+                </li>
+                <div>
+                  <a href="https://github.com/csnye86">
+                    <img className="social" src={githublogo} alt="" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/cameronnye/">
+                    <img className="social" src={linkedinlogo} alt="" />
+                  </a>
+                </div>
+                <li className='mobileSite'>*Mobile site coming soon</li>
+              </ul>
             </div>
           </div>
           <div className={this.state.toggleMenu ? "body" : "hide"}>
